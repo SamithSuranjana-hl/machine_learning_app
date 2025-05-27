62ae8f8efed879c9ee6a2e001f4a5a4a3075ab3a
SamithSuranjana-hl <samithsuranjana84@gmail.com>
2025-05-28 00:54:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,56 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('Machine Learning App')
 
-st.write('Hello world!')
+st.info("Let's make a machine learning model.")
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df 
+
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  X
+
+  st.write('**y**') 
+  y = df.species
+  y
+
+with st.expander('Data Visualization'):
+  # "bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
+  st.scatter_chart(data = df , x = 'bill_length_mm' , y = 'body_mass_g' , color = 'species' )
+
+
+# data preparation
+
+with st.sidebar:
+  st.header('Input Features')
+  # "island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  island = st.selectbox('island', ('Torgersen', 'Biscoe', 'Dream') )
+  sex = st.selectbox('sex', ('male', 'female'))
+  bill_length_mm = st.slider('bill_length_mm', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('bill_depth_mm', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('flipper_length_mm', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('body_mass_g', 2700.0, 6300.0, 4207.0 )
+
+# create a dataframe for the input features.
+  data = {
+    'island': island,
+    'bill_length_mm': bill_length_mm,
+    'bill_depth_mm': bill_depth_mm,
+    'flipper_length_mm': flipper_length_mm,
+    'body_mass_g': body_mass_g,
+    'sex': sex
+  }
+
+
+  input_df = pd.DataFrame(data, index = [0])
+  input_penguins = pd.concat([input_df,X], axis=0)
+  
+with st.expander('input features'):
+  st.write('**input penguin**')
+  input_df
+  st.write('**combined penguin data**')
+  input_penguins
