aae34a2b7d4f772dd562bf1f45dcbc87a544f928
SamithSuranjana-hl <samithsuranjana84@gmail.com>
2025-05-29 18:34:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ st.title('Machine Learning App')
 
 st.info("Let's make a machine learning model.")
 
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df 
 
   st.write('**X**')
   X = df.drop('species', axis=1)
